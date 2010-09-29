a5fa62ee84568ebad493eb8bf551a2c24ff94867
Nat Weerawan <nat.wrw@gmail.com>
2010-09-29 12:33:02 +0700

    Add newapp --myapp

--- myapp/views.py
+++ myapp/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index(request):
+    return render_to_response('myapp/index.html', {}); 
+   
+
